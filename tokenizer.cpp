4f371a4f88373c3c3fa3280f9b53d4bbf1e3e938
Daniel <bjt445@gmail.com>
2024-02-21 19:14:02 -0800

    String and INT cases added

--- tokenizer.cpp
+++ tokenizer.cpp
@@ -10,29 +10,40 @@ enum class EIdentifier
 	state_2, // finish with empty space
 };
 
+enum class EString
+{
+	state_1, // Starts with "
+	state_2, // String
+	state_3, // Ends with "
+};
 
-std::string getTokenKind(char input, bool* bToken) {
+
+std::string getTokenKind(char input, bool* bToken = new bool(0)){
 	std::string character = "0";
 	switch (input) {
-	case '(': character = "L_PAREN"; break;
-		case ')': character = "R_PAREN"; break;
-		case '[': character = "L_BRACKET"; break;
-		case ']': character = "R_BRACKET"; break;
-		case '{': character = "L_BRACE"; break;
-		case '}': character = "R_BRACE"; break;
+	case '(': character = "LEFT_PARENTHESIS"; break;
+		case ')': character = "RIGHT_PARENTHESIS"; break;
+		case '[': character = "LEFT_BRACKET"; break;
+		case ']': character = "RIGHT_BRACKET"; break;
+		case '{': character = "LEFT_BRACE"; break;
+		case '}': character = "RIGHT_BRACE"; break;
 		case '"': character = "DOUBLE_QUOTE"; break;
 		case '\'':character = "SINGLE_QUOTE"; break;
 		case ';': character = "SEMICOLON"; break;
 		case ',': character = "COMMA"; break;
-		case '=': character = "ASSIGNMENT_OPERATOR"; break;
+		case '=': character = "ASSIGNMENT"; break;
 		case '+': character = "PLUS"; break;
 		case '-': character = "MINUS"; break;
 		case '*': character = "ASTERISK"; break;
 		case '/': character = "DIVIDE"; break;
 		case '%': character = "MODULO"; break;
 		case '^': character = "CARET"; break;
-		case '<': character = "LT"; break;
-		case '>': character = "GT"; break;
+		case '<': character = "LESS_THAN"; break;
+		case '>': character = "GREATER_THAN"; break;
+		case '>=': character = "GREATER_THAN_OR_EQUAL"; break;
+		case '<=': character = "LESS_THAN_OR_EQUAL"; break;
+		case '&&': character = "BOOLEAN_AND"; break;
+		case '==' : character = "EQUAL"; break;
 		default: character = "0"; break;
 			break;
 	}
@@ -48,14 +59,17 @@ std::string getTokenKind(char input, bool* bToken) {
 	return character;
 }
 
+
+
 template <typename T>
 void PrintToken(std::ofstream* file, T toPrint, std::string tokenType)
 {
 	*file << "Token type: " << tokenType << '\n';
-	*file << "Token: " << toPrint << '\n' << '\n';
+	*file << "Token:      " << toPrint << '\n' << '\n';
 }
 
 EIdentifier IDENTIFIER;
+EString ISSTRING;
 
 
 void GetCharacter(std::ifstream* inFile, std::ofstream* outFile)
@@ -63,6 +77,25 @@ void GetCharacter(std::ifstream* inFile, std::ofstream* outFile)
 	char c;
 	while (inFile->get(c))
 	{
+		// Check for integers
+		if (isdigit(c) || (c == '-' && isdigit(inFile->peek())))
+		{
+			std::string token;
+			token += c;
+			bool looping = true;
+			while (looping)
+			{
+				inFile->get(c);
+				if (!isdigit(c))
+				{
+					looping = false;
+					break;
+				}
+				token += c;
+			}
+			PrintToken(outFile, token, "INTEGER");
+		}
+		
 		// Check for Identifiers
 		// From A - Z and a - z
 		if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
@@ -87,7 +120,6 @@ void GetCharacter(std::ifstream* inFile, std::ofstream* outFile)
 
 			}
 			IDENTIFIER = EIdentifier::state_2;
-			std::cout << token;
 			PrintToken(outFile, token, "IDENTIFIER");
 		}
 
@@ -99,6 +131,27 @@ void GetCharacter(std::ifstream* inFile, std::ofstream* outFile)
 			PrintToken(outFile, c, tokenType);
 		}
 
+		// Check for strings
+		if (c == '"')
+		{
+			ISSTRING = EString::state_1;
+			std::string inString = "";
+			while (inFile->get(c))
+			{
+				ISSTRING = EString::state_2;
+				if (c == '"')
+				{
+					ISSTRING = EString::state_3;
+					break;
+				}
+				inString += c;
+			}
+			PrintToken(outFile, inString, "STRING"); // Print the string in the file
+			PrintToken(outFile, c, getTokenKind(c)); // Print the second " in the file
+
+		}
+
+		
 
 	}
 
@@ -112,12 +165,12 @@ int main()
 	std::ofstream outputStream;
 
 	std::string fileName;
-	std::string outputFile = "myoutput.txt";
+	std::string outputFile = "myoutput2.txt";
 
 	std::cout << "Enter name of file" << std::endl;
 	//std::cin >> fileName;
 
-	fileName = "test1.c";
+	fileName = "test2.c";
 
 
 	//open file
@@ -137,7 +190,7 @@ int main()
 	}
 
 
-	outputStream << "Token List: " << '\n' << '\n';
+	outputStream << "Token list: " << '\n' << '\n';
 	GetCharacter(&fileStream, &outputStream);
 
 }
